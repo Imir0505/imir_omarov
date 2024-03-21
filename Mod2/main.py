3721d83a5ca96eeacd59845384bb25b9b7fd8f6b
Imir0505 <146982805+Imir0505@users.noreply.github.com>
2024-03-21 17:58:12 +0500

    Add files via upload

---
+++ Mod2/main.py
@@ -0,0 +1,62 @@
+###Задание 1
+def get_summary_rss(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()[1☺ # Пропускаем первую строку с заголовком
+        total_bytes = 0
+
+    for line in lines:
+        columns = line.split()
+        rss = int(columns[5]) # Получаем значение столбца rss
+
+        total_bytes += rss
+
+        units = ['б', 'кб', 'мб', 'гб']
+        unit_index = 0
+
+    while total_bytes >= 1024 and unit_index < len(units) - 1:
+        total_bytes /= 1024
+        unit_index += 1
+
+    return f"{total_bytes} {units[unit_index]}"
+
+    if __name__ == "__main__":
+        file_path = "output_file.txt"
+        summary_rss = get_summary_rss(file_path)
+print(summary_rss)
+
+###Задание 2
+def get_mean_size(data):
+    sizes = []
+    for line in data:
+        words = line.split()
+    if len(words) >= 5:
+        size = int(words[4])
+        sizes.append(size)
+
+    if sizes:
+        mean_size = sum(sizes) / len(sizes)
+        return mean_size
+
+    return 0
+
+###Задание 3
+def decrypt(cipher):
+    decrypted = []
+    i = 0
+
+    while i < len(cipher):
+        if cipher[i].isalpha():
+            decrypted.append(cipher[i])
+            i += 1
+        elif cipher[i:i+2] == '..':
+            decrypted.pop()
+            i += 2
+        else:
+            i += 1
+
+    return ''.join(decrypted)
+
+import sys
+
+for line in sys.stdin:
+    print(decrypt(line.strip()))
