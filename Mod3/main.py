0ce2dee15dd2b11afe038e454ca3a885f7bcfca8
Имир <146982805+Imir0505@users.noreply.github.com>
2024-08-11 21:55:07 +0300

    Update main.py

--- Mod3/main.py
+++ Mod3/main.py
@@ -115,3 +115,35 @@ print("http://127.0.0.1:5000/calculate/2024/7")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+###Задание 4
+import datetime
+
+
+class Person:
+    def __init__(self, name, year_of_birth, address=''):
+        self.name = name
+        self.yob = year_of_birth
+        self.address = address
+
+    def get_age(self):
+        now = datetime.datetime.now()
+        return now.year - self.yob
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_address(self, address):
+        self.address = address
+
+    def get_address(self):
+        return self.address
+
+    def is_homeless(self):
+        '''
+        returns True if address is not set, false in other case
+        '''
+        return self.address is ""
