1f3eb9cc0e338ac24fb490be143368eeebf4714a
Имир <146982805+Imir0505@users.noreply.github.com>
2024-08-11 21:30:17 +0300

    Update main.py

--- Mod3/main.py
+++ Mod3/main.py
@@ -65,3 +65,53 @@ class TestDecoder(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+###Задание 3 
+from flask import Flask, jsonify
+from collections import defaultdict
+
+app = Flask(__name__)
+
+storage = defaultdict(lambda: defaultdict(int))
+storage[2022][1] = 250
+storage[2022]['total'] += 250
+
+
+@app.route('/add/<date>/<int:number>')
+def add_expense(date, number):
+    try:
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[6:])
+        if len(date) > 8:
+            raise ValueError
+    except ValueError:
+        return 'Неверный формат даты'
+
+    storage[year][month] += number
+    storage[year]['total'] += number
+
+    return "Затраты добавлены."
+
+
+@app.route('/calculate/<int:year>')
+def calculate_year(year):
+    if year in storage:
+        return f"Суммарные траты за {year} год: {storage[year]['total']}"
+    else:
+        return "Нет данных о затратах за указанный год."
+
+
+@app.route('/calculate/<int:year>/<int:month>')
+def calculate_month(year, month):
+    if year in storage and month in storage[year]:
+        return f"Суммарные траты за {month}.{year}: {storage[year][month]}"
+    else:
+        return "Нет данных о затратах за указанный месяц."
+
+print("http://127.0.0.1:5000/add/20240705/300")
+print("http://127.0.0.1:5000/calculate/2024")
+print("http://127.0.0.1:5000/calculate/2024/7")
+
+if __name__ == '__main__':
+    app.run(debug=True)
