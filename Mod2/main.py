4aab28084be192bc3e24ac3f9fd3dfd6a1ad68e8
Имир <146982805+Imir0505@users.noreply.github.com>
2024-08-11 19:05:34 +0300

    Update main.py

--- Mod2/main.py
+++ Mod2/main.py
@@ -138,6 +138,55 @@ def preview(size, relative_path):
     except FileNotFoundError:
         return "Файл не найден."
 
+print("http://127.0.0.1:5000/preview/5/task2.6_file.txt")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+###Задание 7
+from flask import Flask, jsonify
+from collections import defaultdict
+
+app = Flask(__name__)
+
+storage = defaultdict(lambda: defaultdict(int))
+
+
+@app.route('/add/<date>/<int:number>')
+def add_expense(date, number):
+    try:
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[6:])
+        if len(date) > 8:
+            raise ValueError
+    except ValueError:
+        return jsonify({'error': 'Неверный формат даты'}), 400
+
+    storage[year][month] += number
+    storage[year]['total'] += number
+
+    return "Затраты добавлены."
+
+
+@app.route('/calculate/<int:year>')
+def calculate_year(year):
+    if year in storage:
+        return f"Суммарные траты за {year} год: {storage[year]['total']}"
+    else:
+        return "Нет данных о затратах за указанный год."
+
+
+@app.route('/calculate/<int:year>/<int:month>')
+def calculate_month(year, month):
+    if year in storage and month in storage[year]:
+        return f"Суммарные траты за {month}.{year}: {storage[year][month]}"
+    else:
+        return "Нет данных о затратах за указанный месяц."
+
+print("http://127.0.0.1:5000/add/20240705/300")
+print("http://127.0.0.1:5000/calculate/2024")
+print("http://127.0.0.1:5000/calculate/2024/7")
 
 if __name__ == '__main__':
     app.run(debug=True)
