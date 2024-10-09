c6fef937e97b1d6edd722590c82dcc12a38ab530
Имир <146982805+Imir0505@users.noreply.github.com>
2024-10-09 10:18:24 +0500

    Update test_main.py

--- Mod3/test_main.py
+++ Mod3/test_main.py
@@ -1,1 +1,72 @@
+# Задание 1
+import unittest
+from freezegun import freeze_time
+from main import app
 
+
+class TestHelloWorldEndpoint(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.base_url = '/hello-world/'
+
+    @freeze_time("2024-03-14")
+    def test_hello_world_with_name_thursday(self):
+        response = self.app.get('/hello-world/Alice')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Alice. Хорошего четверга!', text)
+
+    @freeze_time("2024-03-15")
+    def test_hello_world_with_name_friday(self):
+        response = self.app.get('/hello-world/Bob')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Bob. Хорошей пятницы!', text)
+
+    @freeze_time("2024-03-16")
+    def test_hello_world_without_name_saturday(self):
+        response = self.app.get('/hello-world/Alex')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Alex. Хорошей субботы!', text)
+
+    @freeze_time("2024-03-17")
+    def test_hello_world_without_name_sunday(self):
+        response = self.app.get('/hello-world/Alex')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Alex. Хорошего воскресенья!', text)
+
+    @freeze_time("2024-03-18")
+    def test_hello_world_without_name_monday(self):
+        response = self.app.get('/hello-world/Alex')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Alex. Хорошего понедельника!', text)
+
+    @freeze_time("2024-03-19")
+    def test_hello_world_without_name_tuesday(self):
+        response = self.app.get('/hello-world/Alex')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Alex. Хорошего вторника!', text)
+
+    @freeze_time("2024-03-20")
+    def test_hello_world_without_name_wednesday(self):
+        response = self.app.get('/hello-world/Alex')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Alex. Хорошей среды!', text)
+
+    @freeze_time("2024-03-20")
+    def test_hello_world_without_name_correct(self):
+        response = self.app.get('/hello-world/Хорошей среды')
+        text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Привет, Хорошей среды. Хорошей среды!', text)
+
+if __name__ == '__main__':
+    unittest.main()
