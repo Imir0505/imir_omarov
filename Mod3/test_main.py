1a5eb0c058672bb63d9f73a86432dddb28c1ad7e
Имир <146982805+Imir0505@users.noreply.github.com>
2024-10-09 19:19:45 +0500

    Update test_main.py

--- Mod3/test_main.py
+++ Mod3/test_main.py
@@ -1,3 +1,5 @@
+# Запуск тестов:
+# python -m unittest test_main.py
 # Задание 1
 import unittest
 from freezegun import freeze_time
@@ -122,6 +124,47 @@ class TestFinanceApp(unittest.TestCase):
         response = self.app.get('/calculate/2022/2')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode(), 'Нет данных о затратах за указанный месяц.')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Задание 4
+import unittest
+from datetime import datetime
+from main import Person
+
+
+class TestPerson(unittest.TestCase):
+
+    def setUp(self):
+        self.person = Person('John', 1990, '123 Main St')
+
+    def test_get_age(self):
+        current_year = datetime.now().year
+        expected_age = current_year - 1990
+        self.assertEqual(self.person.get_age(), expected_age)
+
+    def test_get_name(self):
+        self.assertEqual(self.person.get_name(), 'John')
+
+    def test_set_name(self):
+        self.person.set_name('Jane')
+        self.assertEqual(self.person.get_name(), 'Jane')
+
+    def test_set_address(self):
+        self.person.set_address('456 Elm St')
+        self.assertEqual(self.person.get_address(), '456 Elm St')
+
+    def test_get_address(self):
+        self.assertEqual(self.person.get_address(), '123 Main St')
+
+    def test_is_homeless(self):
+        self.assertFalse(self.person.is_homeless())
+
+    def test_is_homeless_when_address_not_set(self):
+        homeless_person = Person('Homeless', 1980)
+        self.assertTrue(homeless_person.is_homeless())
 
 
 if __name__ == '__main__':
