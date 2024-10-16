2fbedef3e6917dd8f77e7a92eaef2fdd22a9c71a
Имир <146982805+Imir0505@users.noreply.github.com>
2024-10-16 15:12:17 +0500

    Update dict_config.py

--- Mod7/dict_config.py
+++ Mod7/dict_config.py
@@ -35,3 +35,48 @@ dict_config = {
         },
     },
 }
+
+# Задание 7
+import logging
+
+
+class AsciiFilter(logging.Filter):
+    def filter(self, record):
+        return 1 if record.message.encode().isascii() else 0
+
+dict_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "base": {
+            "format": "%(levelname)s | %(name)s | %(asctime)s | %(lineno)s | %(message)s"
+        }
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "level": "DEBUG",
+            "formatter": "base",
+            "filename": "calculate.log",
+            "encoding": "utf-8"
+        },
+        "timed": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "level": "DEBUG",
+            "formatter": "base",
+            "filename": "calculate.log",
+            "when": "H",
+            "interval": 10,
+            "backupCount": 0,
+            "encoding": "utf-8"
+        }
+    },
+    "loggers": {
+        "Calculate": {
+            "level": "DEBUG",
+            "handlers": ["file"],
+            "filter": AsciiFilter
+        }
+    },
+
+}
