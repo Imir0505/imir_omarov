e5a347e4911dc1fa4a1644746328fbd0994cab7c
Имир <146982805+Imir0505@users.noreply.github.com>
2024-10-22 16:15:37 +0500

    Add files via upload

---
+++ Mod12/server.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/timestamp/<timestamp>')
+def get_timestamp(timestamp: str) -> str:
+    timestamp = float(timestamp)
+    return str(datetime.fromtimestamp(timestamp))
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=8080)
